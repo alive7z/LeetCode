ac9cf6c7843de5ec81bc1b0775ffd76f79f5c8e8
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-01 16:49:42 +0530

    LC-minimum-cost-of-buying-candies-with-discount [Runtime: N/A; Memory: N/A]

--- sorting/minimum-cost-of-buying-candies-with-discount.cpp
+++ sorting/minimum-cost-of-buying-candies-with-discount.cpp
@@ -3,8 +3,8 @@ public:
     int minimumCost(vector<int>& cost) {
         int sum = 0;
         sort(cost.rbegin(), cost.rend());
-        for(int i = 0; i<cost.size(); i++) {
-            if((i+1) % 3 != 0) {
+        for(int i = 0; i < cost.size(); i++) {
+            if((i + 1) % 3 != 0) {
                 sum += cost[i];
             }
         }
