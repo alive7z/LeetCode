892d4824805ae87bb9b9519dfcbb3d57813bca23
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-02 12:33:04 +0530

    LC-pascals-triangle [Runtime: N/A; Memory: N/A]

--- dynamic-programming/pascals-triangle.cpp
+++ dynamic-programming/pascals-triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+    vector<int> generateRow(int row) {
+        long long ans = 1;
+        vector<int> temp;
+        temp.push_back(1);
+        for(int col = 1; col < row; col ++) {
+            ans = ans * (row - col);
+            ans /= col;
+            temp.push_back(ans);
+        }
+        return temp;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i = 1; i <= numRows; i++) {
-            vector<int> temp;
-            long long val = 1;
-            temp.push_back(val);
-            for(int j = 1; j < i; j++) {
-                val = val * (i - j);
-                val = val / j;
-                temp.push_back(val);
-            }
+        for(int row = 1; row <= numRows; row++) {
+            vector<int> temp = generateRow(row);
             ans.push_back(temp);
         }
         return ans;
