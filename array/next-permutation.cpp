64a33a0e726b165215e5950b612de5c049443cf7
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-02 15:44:53 +0530

    LC-next-permutation [Runtime: N/A; Memory: N/A]

--- array/next-permutation.cpp
+++ array/next-permutation.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int pivot = -1;
-        for(int i = n-2; i>=0; i--) {
-            if(nums[i] < nums[i+1]) {
-                pivot = i;
-                break;
+
+        if(nums.size() < 2) return;
+
+        int i = nums.size() - 1, j = nums.size() - 1;
+
+        if(nums[i - 1] < nums[i]) {
+            swap(nums[i-1], nums[i]);
+        } else {
+            while(j - 1 >= 0 && nums[j] < nums[j-1]) {
+                j--;
             }
-        }
-        if(pivot == -1) {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        for(int i = n-1; i>pivot; i--) {
-            if(nums[i] > nums[pivot]) {
-                swap(nums[i], nums[pivot]);
-                break;
-            }
-        }
-        int i = pivot + 1, j = n-1;
-        while(i<=j) {
-            swap(nums[i++], nums[j--]);
+            swap(nums[i], nums[j]);
+            sort(nums.begin() + j, nums.end());
         }
     }
 };
