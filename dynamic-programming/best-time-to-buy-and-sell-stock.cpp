06aa4ace885f7cc265df46d7c91842b71940b1e3
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-03 17:17:27 +0530

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- dynamic-programming/best-time-to-buy-and-sell-stock.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int bestBuy = prices[0], totalProfit = 0;
-        for(int i = 1; i<prices.size(); i++) {
-            if(bestBuy > prices[i]) {
-                bestBuy = prices[i];
-                totalProfit = 0;
-                continue;
-            }
+        for(int i = 1; i < prices.size(); i++) {
+            bestBuy = min(bestBuy, prices[i]);
             totalProfit = max(totalProfit, prices[i] - bestBuy);
         }
         return totalProfit;
