6bea1c875a6459e7bebe0e87c0dfc047a10392fd
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-04 17:27:26 +0530

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- array/merge-intervals.cpp
+++ array/merge-intervals.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
+        if(intervals.size() < 2) return intervals;
         sort(intervals.begin(), intervals.end());
         for(int i = 1; i < intervals.size(); i++) {
             if(intervals[i - 1][1] >= intervals[i][0]) {
