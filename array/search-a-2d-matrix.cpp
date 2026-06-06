d891f55da91df0ca17e6a51e872d5e66d5db27eb
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-06 19:55:39 +0530

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- array/search-a-2d-matrix.cpp
+++ array/search-a-2d-matrix.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    bool searchRow(vector<vector<int>>& matrix, int target, int row) {
-        int n = matrix[0].size();
-        int st = 0, end = n - 1;
-        while(st <= end) {
-            int mid = st + (end - st) / 2;
-            if(target == matrix[row][mid]) {
-                return true;
-            } else if(target > matrix[row][mid]) {
-                st = mid + 1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int first = 0, second = -1;
+        int row = matrix.size() - 1;
+        int col = matrix[0].size() - 1;
+
+        while(first <= row) {
+            int mid = first + (row - first) / 2;
+            if(target >= matrix[mid][0] && target <= matrix[mid][col]) {
+                second = mid;
+                break;
+            } else if(target > matrix[mid][0] && target > matrix[mid][col]){
+                first = mid + 1;
             } else {
-                end = mid - 1;
+                row = mid - 1;
             }
         }
-        return false;
-    }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        int startRow = 0, endRow = m - 1;
-        while(startRow <= endRow) {
-            int midRow = startRow + (endRow - startRow) / 2;
-            if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]) {
-                return searchRow(matrix, target, midRow);
-            } else if(target > matrix[midRow][0]) {
-                startRow = midRow + 1;
+
+        while(second <= col) {
+            int mid = second + (col - second) / 2;
+            if(matrix[second][mid] == target) {
+                return true;
+            } else if(target > matrix[second][mid]) {
+                second = mid + 1;
             } else {
-                endRow = midRow - 1;
+                col = mid - 1;
             }
-        } return false;
+        }
+
+        return false;
     }
 };
