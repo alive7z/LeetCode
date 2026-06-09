cfd80ae1ef1d0b78a5c7077cf914e15d3c4ead40
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-09 19:08:12 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.cpp
+++ hash-table/longest-substring-without-repeating-characters.cpp
@@ -5,16 +5,16 @@ public:
         int ans = 0, cnt = 0;
         int i = 0, j = 0;
         while(j < s.length()) {
-            if(check[s[j] - 'a'] == true) {
+            if(check[s[j]] == true) {
                 ans = max(ans, cnt);
                 while(s[i] != s[j]) {
-                    check[s[i++] - 'a'] = false;
+                    check[s[i++]] = false;
                 }
-                check[s[i++] - 'a'] = false;
+                check[s[i++]] = false;
                 cnt = j - i + 1;
-                check[s[j++] - 'a'] = true;
+                check[s[j++]] = true;
             } else {
-                check[s[j++] - 'a'] = true;
+                check[s[j++]] = true;
                 cnt++;
             }
         }
