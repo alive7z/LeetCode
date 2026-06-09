7d74d16cd2ffb846974c5eeb3f5df23f7c5ec597
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-09 17:13:07 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.cpp
+++ array/longest-consecutive-sequence.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         int ans = INT_MIN;
-        int cnt = 0, val;
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i<nums.size(); i++) {
             mp[nums[i]] = i;
         }
-        for(auto itr : mp) {
-            if(cnt == 0) {
-                val = itr.first;
-                cnt = 1;
-            } else {
-                if(itr.first == val + 1) {
-                    cnt++;
-                    val = itr.first;
-                } else {
-                    ans = max(ans, cnt);
-                    cnt = 1;
-                    val = itr.first;
-                }
+        for(int val : nums) {
+            int temp = val, cnt = 1;
+            while(mp.find(temp - 1) != mp.end()) {
+                cnt++;
+                temp -= 1;
             }
+            ans = max(ans, cnt);
         }
-        ans = max(ans, cnt);
         return ans;
     }
 };
