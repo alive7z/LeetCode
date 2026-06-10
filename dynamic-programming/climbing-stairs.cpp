42cdc9083bb9869d1605c72ab41b318489e04807
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-10 16:39:54 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- dynamic-programming/climbing-stairs.cpp
+++ dynamic-programming/climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int func(int n, vector<int>& dp) {
         if(n <= 2) return n;
         if(dp[n] != -1) return dp[n];
-        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return dp[n] =  func(n - 1) + func(n - 2);
     }
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
