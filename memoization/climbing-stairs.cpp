9c1c76b893ce6bef4d4c1b6f6ce1e3e6c1166b46
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-10 16:40:09 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.cpp
+++ memoization/climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int func(int n, vector<int>& dp) {
         if(n <= 2) return n;
         if(dp[n] != -1) return dp[n];
-        return dp[n] =  func(n - 1) + func(n - 2);
+        return dp[n] =  func(n - 1, dp) + func(n - 2, dp);
     }
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
