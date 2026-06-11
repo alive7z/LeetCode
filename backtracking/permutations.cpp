cb36d2787bd3c8c5ef24dd3c2efca9b5d484d099
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-11 23:04:41 +0530

    LC-permutations [Runtime: N/A; Memory: N/A]

---
+++ backtracking/permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void permutations(vector<int>& nums, vector<vector<int>>& ans) {
+        int i = nums.size() - 1;
+        while(i > 0 && nums[i] < nums[i-1]) i--;
+        if(i == 0) {
+            return;
+        }
+        for(int j = nums.size() - 1; j >= i; j--) {
+            if(nums[j] > nums[i-1]) {
+                swap(nums[j], nums[i-1]);
+                break;
+            }
+        }
+        reverse(nums.begin() + i, nums.end());
+        ans.push_back(nums);
+        permutations(nums, ans);
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        ans.push_back(nums);
+        permutations(nums, ans);
+        return ans;
+    }
+};
