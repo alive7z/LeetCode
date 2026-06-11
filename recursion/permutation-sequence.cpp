91d54468d057b06ea94e6763c8b4ea3336e49032
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-11 20:58:01 +0530

    LC-permutation-sequence [Runtime: N/A; Memory: N/A]

--- recursion/permutation-sequence.cpp
+++ recursion/permutation-sequence.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    void func(vector<int>& nums, int k) {
-        if(k == 0) return;
-        int i = nums.size() - 1;
-        while(i > 0 && nums[i] <= nums[i - 1]) {
-            i--;
-        }
-        if(i == 0) return;
-        for(int j = nums.size() - 1; j >= i; j--) {
-            if(nums[j] > nums[i - 1]) {
-                swap(nums[i - 1], nums[j]);
-                break;
-            }
-        }
-        reverse(nums.begin() + i, nums.end());
-        func(nums, k - 1);
-    }
     string getPermutation(int n, int k) {
         vector<int> nums;
-        for(int i = 1; i <= n; i++) {
-            nums.push_back(i);
+        int fact = 1;
+        int i = 1;
+        while(i <= n) {
+            fact *= i;
+            i++;
         }
-        func(nums, k - 1);
+        int div = fact / n;
+        int mod = k % div;
+        int pos = (k / div) + 1;
+        nums.push_back(pos);
+        for(int i = 0; i < n; i++) {
+            if((i+1) != pos) {
+                nums.push_back(i+1);
+            }
+        }
+        for(int k = 0; k < mod - 1; k++) {
+            int i = nums.size() - 1;
+            while(i > 0 && nums[i] < nums[i-1]) i--;
+            for(int j = nums.size() - 1; j >= i; j--) {
+                if(nums[j] > nums[i-1]) {
+                    swap(nums[j], nums[i-1]);
+                    break;
+                }
+            }
+            reverse(nums.begin() + i, nums.end());
+        }
         string str = "";
         for(int val : nums) {
             str += to_string(val);
