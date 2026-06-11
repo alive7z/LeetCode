ab76d3920694303f547da5f9d0ed04d7a619a3a7
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-11 12:39:15 +0530

    LC-combination-sum-ii [Runtime: N/A; Memory: N/A]

---
+++ array/combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void combinations(vector<int>& candidates, set<vector<int>>& st, vector<int> temp, int target, int i) {
+        if(i == candidates.size()) {
+            if(target == 0) {
+                st.insert(temp);
+            }
+            return;
+        }
+        if(candidates[i] <= target) {
+            temp.push_back(candidates[i]);
+            combinations(candidates, st, temp, target - candidates[i], i + 1);
+            temp.pop_back();
+        }
+        combinations(candidates, st, temp, target, i + 1);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        set<vector<int>> st;
+        vector<int> temp;
+        combinations(candidates, st, temp, target, 0);
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
