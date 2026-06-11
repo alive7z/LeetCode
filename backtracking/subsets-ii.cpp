8d3b28cee211fd97eca2612ad5b9a0c230bbb50b
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-11 08:31:20 +0530

    LC-subsets-ii [Runtime: N/A; Memory: N/A]

---
+++ backtracking/subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void subSets(vector<int>& nums, vector<int>& temp, set<vector<int>>& st, int i) {
+        if(i == nums.size()) {
+            st.insert(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        subSets(nums, temp, st, i + 1);
+        temp.pop_back();
+        subSets(nums, temp, st, i + 1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> temp;
+        set<vector<int>> st;
+        subSets(nums, temp, st, 0);
+        vector<vector<int>> ans(st.begin(), st.end());
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
