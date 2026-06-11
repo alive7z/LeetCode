7006282672684a8ee0ad054193a837d6c941fdcf
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-11 12:26:39 +0530

    LC-combination-sum [Runtime: N/A; Memory: N/A]

--- backtracking/combination-sum.cpp
+++ backtracking/combination-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void combinations(vector<int>& candidates, vector<vector<int>>& ans, vector<int> temp, int target, int i, int sum) {
+    void combinations(vector<int>& candidates, vector<int> temp, vector<vector<int>>& ans, int i, int target) {
         if(i == candidates.size()) {
-            if(sum == target) {
-                ans.push_back(temp);
-            }
+            if(target == 0) ans.push_back(temp);
             return;
         }
-        if(sum + candidates[i] <= target) {
+        if(candidates[i] <= target) {
             temp.push_back(candidates[i]);
-            combinations(candidates, ans, temp, target, i, sum + candidates[i]);
+            combinations(candidates, temp, ans, i, target - candidates[i]);
             temp.pop_back();
         }
-        combinations(candidates, ans, temp, target, i + 1, sum);
+        combinations(candidates, temp, ans, i + 1, target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        int sum = 0;
-        combinations(candidates, ans, temp, target, 0, sum);
+        combinations(candidates, temp, ans, 0, target);
         return ans;
     }
 };
