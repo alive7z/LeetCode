0d6fe293b015e35eff3634c8c7395654f5559cbd
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-16 12:45:30 +0530

    LC-process-string-with-special-operations-i [Runtime: N/A; Memory: N/A]

--- string/process-string-with-special-operations-i.cpp
+++ string/process-string-with-special-operations-i.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     string processStr(string s) {
+        int n = s.size();
         string result = "";
-        for(int i = 0; i < s.size(); i++) {
+        for(int i = 0; i < n; i++) {
             if(s[i] == '*') {
                 if(!result.empty()) {
                     result.pop_back();
@@ -12,7 +13,7 @@ public:
             } else if(s[i] == '%') {
                 reverse(result.begin(), result.end());
             } else {
-                result += s[i];
+                result.push_back(s[i]);
             }
         }
         return result;
