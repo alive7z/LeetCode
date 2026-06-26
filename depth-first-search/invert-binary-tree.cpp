6d65c5598431d40697fd440bcc385522833e8431
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-26 09:49:31 +0530

    LC-invert-binary-tree [Runtime: 0 ms; Memory: 12.5 MB]

---
+++ depth-first-search/invert-binary-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        if(root == NULL) return root;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* temp = q.front();
+            q.pop();
+            swap(temp -> left, temp -> right);
+            if(temp -> left != NULL) {
+                q.push(temp -> left);
+            }
+            if(temp -> right != NULL) {
+                q.push(temp -> right);
+            }
+        }
+        return root;
+    }
+};
