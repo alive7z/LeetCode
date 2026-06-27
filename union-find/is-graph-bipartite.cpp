3e03aa8eecbc5c0726e2eccee543ba05973d0e6a
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-27 23:21:31 +0530

    LC-is-graph-bipartite [Runtime: N/A; Memory: N/A]

--- union-find/is-graph-bipartite.cpp
+++ union-find/is-graph-bipartite.cpp
@@ -4,22 +4,25 @@ public:
         int V = graph.size();
         vector<int> color(V, 0);
         queue<int> q;
-        q.push(0);
-        color[0] = 1;
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            for(auto itr : graph[node]) {
-                if(color[itr] == 0) {
-                    if(color[node] == 1) {
-                        color[itr] = 2;
-                        q.push(itr);
-                    } else if(color[node] == 2) {
-                        color[itr] = 1;
-                        q.push(itr);
+        for(int i = 0; i < V; i++) {
+            if(color[i] > 0) continue;
+            q.push(i);
+            color[i] = 1;
+            while(!q.empty()) {
+                int node = q.front();
+                q.pop();
+                for(auto itr : graph[node]) {
+                    if(color[itr] == 0) {
+                        if(color[node] == 1) {
+                            color[itr] = 2;
+                            q.push(itr);
+                        } else if(color[node] == 2) {
+                            color[itr] = 1;
+                            q.push(itr);
+                        }
+                    } else if(color[itr] == color[node]) {
+                        return false;
                     }
-                } else if(color[itr] == color[node]) {
-                    return false;
                 }
             }
         }
