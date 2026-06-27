731866ae4467b75d16a52ef2edd5ca1946ccd1cf
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-27 23:38:00 +0530

    LC-is-graph-bipartite [Runtime: N/A; Memory: N/A]

--- union-find/is-graph-bipartite.cpp
+++ union-find/is-graph-bipartite.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>& graph, vector<int>& color, int node) {
+        for(auto itr : graph[node]) {
+            if(color[itr] == -1) {
+                color[itr] = !color[node];
+                if(!dfs(graph, color, itr)) return false;
+            } else if(color[itr] == color[node]) return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
         vector<int> color(V, -1);
-        queue<int> q;
         for(int i = 0; i < V; i++) {
-            if(color[i] > 0) continue;
-            q.push(i);
-            color[i] = 0;
-            while(!q.empty()) {
-                int node = q.front();
-                q.pop();
-                for(auto itr : graph[node]) {
-                    if(color[itr] == -1) {
-                        color[itr] = !color[node];
-                        q.push(itr);
-                    } else if(color[itr] == color[node]) {
-                        return false;
-                    }
-                }
+            if(color[i] == -1) {
+                color[i] = 0;
+                if(!dfs(graph, color, i)) return false;
             }
         }
         return true;
