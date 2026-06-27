6a3662030fb6dca950baf120804d8b46149646ad
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-27 22:12:47 +0530

    LC-number-of-islands [Runtime: N/A; Memory: N/A]

--- array/number-of-islands.cpp
+++ array/number-of-islands.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j) {
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' || visited[i][j] == true) {
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int i, int j, int m, int n, int& count) {
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0' || visited[i][j]) {
             return;
         }
         visited[i][j] = true;
-        dfs(grid, visited, i + 1, j);
-        dfs(grid, visited, i - 1, j);
-        dfs(grid, visited, i, j + 1);
-        dfs(grid, visited, i, j - 1);
+        dfs(grid, visited, i + 1, j, m, n, count);
+        dfs(grid, visited, i - 1, j, m, n, count);
+        dfs(grid, visited, i, j + 1, m, n, count);
+        dfs(grid, visited, i, j - 1, m, n, count);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int row = grid.size(), col = grid[0].size(), islands = 0;
-        vector<vector<bool>> visited(row, vector<bool> (col));
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(grid, visited, i, j);
-                    islands++;
+        int count = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j] && grid[i][j] == '1') {
+                    dfs(grid, visited, i, j, m, n, count);
+                    count++;
                 }
             }
         }
-        return islands;
+        return count;
     }
 };
