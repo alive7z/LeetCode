76db1a41033a258d6c3b4d47833f379588ba3bac
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-28 00:22:42 +0530

    LC-flood-fill [Runtime: N/A; Memory: N/A]

--- breadth-first-search/flood-fill.cpp
+++ breadth-first-search/flood-fill.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int val) {
-        if(sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] == color || image[sr][sc] != val) return;
+    void dfs(vector<vector<int>>& image, int sr, int sc, int m, int n, int color) {
+        if(sr < 0 || sr >= m || sc < 0 || sc >= n || image[sr][sc] == 0 || image[sr][sc] == color) return;
         image[sr][sc] = color;
-        dfs(image, sr + 1, sc, color, val);
-        dfs(image, sr - 1, sc, color, val);
-        dfs(image, sr, sc + 1, color, val);
-        dfs(image, sr, sc - 1, color, val);
+        dfs(image, sr - 1, sc, m, n, color);
+        dfs(image, sr + 1, sc, m, n, color);
+        dfs(image, sr, sc - 1, m, n, color);
+        dfs(image, sr, sc + 1, m, n, color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int val = image[sr][sc];
-        dfs(image, sr, sc, color, val);
+        int m = image.size();
+        int n = image[0].size();
+        dfs(image, sr, sc, m, n, color);
         return image;
     }
 };
