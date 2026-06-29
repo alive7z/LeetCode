7c0e73a49227e07b0f7e726ee56b590fad4395ec
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-29 21:49:48 +0530

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- array/house-robber.cpp
+++ array/house-robber.cpp
@@ -4,11 +4,9 @@ public:
         int n = nums.size() - 1;
         vector<int> dp(n + 1, -1);
         dp[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++) {
-            int rob = INT_MIN;
-            if(i > 1) {
-                rob = nums[i] + dp[i - 2];
-            }
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); i++) {
+            int rob = nums[i] + dp[i - 2];
             int skip = 0 + dp[i - 1];
             dp[i] = max(rob, skip);
         }
