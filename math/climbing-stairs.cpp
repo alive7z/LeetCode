da433f50b6aa5f868564c32018e4959484d7a633
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-29 16:15:35 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.cpp
+++ math/climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 1) return 1;
-        int a = 1, b = 1, c;
+        if(n == 0 || n == 1) return 1;
+        int prev2 = 1;
+        int prev1 = 1;
+        int curr;
         for(int i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return c;
+        return curr;
     }
 };
