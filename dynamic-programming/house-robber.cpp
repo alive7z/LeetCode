6b3cb8e3751ab481477270ef83ecbd355d736530
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-29 21:50:36 +0530

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.cpp
+++ dynamic-programming/house-robber.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size() - 1;
+        if (n == 1) return nums[0];
         vector<int> dp(n + 1, -1);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
