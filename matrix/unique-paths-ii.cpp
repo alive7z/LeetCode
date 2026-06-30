7a44ae46c37db51b5c1b02d5ca1faea79d72a087
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 20:37:52 +0530

    LC-unique-paths-ii [Runtime: 0 ms; Memory: 11.6 MB]

--- matrix/unique-paths-ii.cpp
+++ matrix/unique-paths-ii.cpp
@@ -6,21 +6,22 @@ public:
         if(obstacleGrid[0][0] == 1) return 0;
         int mod = (int) 2 * 1e9;
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = 1;
+        vector<int> prev(n, 0);
         
         for(int i = 0; i < m; i++) {
+            vector<int> curr(n, 0);
             for(int j = 0; j < n; j++) {
-                if(i == 0 && j == 0) continue;
-                else if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                if(i == 0 && j == 0) curr[j] = 1;
+                else if(obstacleGrid[i][j] == 1) curr[j] = 0;
                 else {
                     int left = 0, up = 0;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = curr[j-1];
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1] % mod;
+        return prev[n-1] % mod;
     }
 };
