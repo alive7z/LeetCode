93056c73f97fced77e284462ce784249b2e40d6f
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 23:57:57 +0530

    LC-triangle [Runtime: N/A; Memory: N/A]

--- array/triangle.cpp
+++ array/triangle.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp(m, vector<int> (m, 0));
+        vector<int> front(m, 0);
         for(int i = 0; i < m; i++) {
-            dp[m-1][i] = triangle[m-1][i];
+            front[i] = triangle[m-1][i];
         }
         for(int i = m - 2; i >= 0; i--) {
+            vector<int> curr(m, 0);
             for(int j = i; j >= 0; j--) {
-                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+                curr[j] = triangle[i][j] + min(front[j], front[j+1]);
             }
+            front = curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
