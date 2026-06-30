a49127aa02c507a9a194b19335b3019d0655676b
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 20:48:56 +0530

    LC-minimum-path-sum [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/minimum-path-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int combinations(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j, int m, int n) {
+        if(i >= m || j >= n) return 1e9;
+        if(i == m - 1 && j == n - 1) {
+            return dp[i][j] = grid[i][j];
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = grid[i][j] + min(combinations(grid, i + 1, j, m, n), combinations(grid, i, j + 1, m, n));
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return combinations(grid, dp, 0, 0, m, n);
+    }
+};
