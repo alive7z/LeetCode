a96c2a13e3d0a81b67f96a52be729bb23dea298a
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 20:59:26 +0530

    LC-minimum-path-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/minimum-path-sum.cpp
+++ dynamic-programming/minimum-path-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int combinations(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j, int m, int n) {
-        if(i >= m || j >= n) return 1e9;
-        if(i == m - 1 && j == n - 1) {
-            return dp[i][j] = grid[i][j];
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = grid[i][j] + min(combinations(grid, dp, i + 1, j, m, n), combinations(grid, dp, i, j + 1, m, n));
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return combinations(grid, dp, 0, 0, m, n);
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(i == m - 1 && j == n - 1) dp[m-1][n-1] = grid[m-1][n-1];
+                else if (i == m - 1) dp[i][j] = grid[i][j] + dp[i][j + 1];
+                else if (j == n - 1) dp[i][j] = grid[i][j] + dp[i + 1][j];
+                else dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
