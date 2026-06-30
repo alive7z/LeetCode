807328ad84c2162491084680955149a247f3cceb
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 19:58:05 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- array/unique-paths-ii.cpp
+++ array/unique-paths-ii.cpp
@@ -3,7 +3,8 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        
         vector<vector<int>> dp(m, vector<int> (n, 0));
         dp[m-1][n-1] = 1;
         for (int j = n - 2; j >= 0; j--) {
@@ -20,7 +21,7 @@ public:
 
         for(int i = m - 2; i >= 0; i--) {
             for(int j = n - 2; j >= 0; j--) {
-                if(obstacleGrid[i][j] == 0 || obstacleGrid[m-1][n-1] == 1) {
+                if(obstacleGrid[i][j] == 0) {
                     dp[i][j] = dp[i+1][j] + dp[i][j+1];
                 } else {
                     dp[i][j] = 0;
