8f79f73a80262e7f1a02920f13a43415ae7088db
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 19:56:35 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- array/unique-paths-ii.cpp
+++ array/unique-paths-ii.cpp
@@ -20,7 +20,7 @@ public:
 
         for(int i = m - 2; i >= 0; i--) {
             for(int j = n - 2; j >= 0; j--) {
-                if(obstacleGrid[i][j] == 0) {
+                if(obstacleGrid[i][j] == 0 || obstacleGrid[m-1][n-1] == 1) {
                     dp[i][j] = dp[i+1][j] + dp[i][j+1];
                 } else {
                     dp[i][j] = 0;
