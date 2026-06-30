5490490a6ae0b0925cd53167227771663b07dd8a
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 16:54:59 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

--- math/unique-paths.cpp
+++ math/unique-paths.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, 1));
+        vector<int> dp(n, 1);
         for(int i = m - 2; i >= 0; i--) {
             for(int j = n - 2; j >= 0; j--) {
-                    dp[i][j] = dp[i][j+1] + dp[i+1][j];
+                    dp[j] = dp[j+1] + dp[j];
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
