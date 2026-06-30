9c28cfca446a329a54d7ad7efbd28380c955a886
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 23:49:57 +0530

    LC-triangle [Runtime: N/A; Memory: N/A]

--- array/triangle.cpp
+++ array/triangle.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int minimumPath(vector<vector<int>>& triangle, int i, int j, int m, int n) {
-        if(i == m - 1) return triangle[i][j];
-        int mini = 0;
-        int adjacent = triangle[i][j] + minimumPath(triangle, i + 1, j, m, n);
-        int diagonal = triangle[i][j] + minimumPath(triangle, i + 1, j + 1, m, n);
-        return mini = min(adjacent, diagonal);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        int n = triangle[0].size();
-        return minimumPath(triangle, 0, 0, m, n);
+        vector<vector<int>> dp(m, vector<int> (m, 0));
+        for(int i = 0; i < m; i++) {
+            dp[m-1][i] = triangle[m-1][i];
+        }
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
