dc7cea178e52ade69f1d7000acf6951fabe8bd5a
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 16:11:48 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths.cpp
+++ dynamic-programming/unique-paths.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int combinations(int i, int j, int m, int n) {
+    int combinations(vector<vector<int>>& dp, int i, int j, int m, int n) {
         if(i == m - 1 && j == n - 1) {
-            return 1;
+            return dp[i][j] = 1;
         }
         if(i >= m || j >= n) {
             return 0;
         }
-        int right = combinations(i + 1, j, m, n);
-        int bottom = combinations(i, j + 1, m, n);
-        return right + bottom;
+        if(dp[i][j] != -1) return dp[i][j];
+        int right = combinations(dp, i + 1, j, m, n);
+        int bottom = combinations(dp, i, j + 1, m, n);
+        return dp[i][j] = right + bottom;
     }
     int uniquePaths(int m, int n) {
-        return combinations(0, 0, m, n);
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return combinations(dp, 0, 0, m, n);
     }
 };
