ecd6319fc8faaabb0ad26cdac7c9aba498fff7db
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 17:52:57 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- array/unique-paths-ii.cpp
+++ array/unique-paths-ii.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int combinations(vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp, int i, int j, int m, int n) {
-        if(i >= m || j >= n || obstacleGrid[i][j] == 1) return 0;
-        if(i == m - 1 && j == n - 1) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = combinations(obstacleGrid, dp, i + 1, j, m, n) + combinations(obstacleGrid, dp, i, j + 1, m, n);
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return combinations(obstacleGrid, dp, 0, 0, m, n);
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        dp[m-1][n-1] = 1;
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j = n - 2; j >= 0; j--) {
+                if(obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
