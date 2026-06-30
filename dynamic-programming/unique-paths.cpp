bd8d210fdc18bfcd15f325741fc93331a3b27148
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 16:32:43 +0530

    LC-unique-paths [Runtime: 2 ms; Memory: 9.4 MB]

--- dynamic-programming/unique-paths.cpp
+++ dynamic-programming/unique-paths.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        for(int i = m - 1; i >= 0; i++) {
-            for(int j = n - 1; j >= 0; j++) {
-                if(i == m - 1 && j == n - 1) {
-                    dp[i][j] = 1;
-                } else {
-                    int right = (i < m - 1) ? dp[i+1][j] : 0;
-                    int bottom = (j < n - 1) ? dp[i][j+1] : 0;
-                    dp[i][j] = right + bottom;
-                }
+        vector<vector<int>> dp(m, vector<int> (n, 1));
+        for(int i = m - 2; i >= 0; i--) {
+            for(int j = n - 2; j >= 0; j--) {
+                    dp[i][j] = dp[i][j+1] + dp[i+1][j];
             }
         }
         return dp[0][0];
