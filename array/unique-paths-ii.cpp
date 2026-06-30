68b16919423bd7ae56ec2fb75aed2eda34651ccd
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 20:14:53 +0530

    LC-unique-paths-ii [Runtime: 0 ms; Memory: 11.6 MB]

--- array/unique-paths-ii.cpp
+++ array/unique-paths-ii.cpp
@@ -3,31 +3,22 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
         
-        vector<vector<int>> dp(m, vector<int> (n, 0));
-        dp[m-1][n-1] = 1;
-        for (int j = n - 2; j >= 0; j--) {
-            if (obstacleGrid[m - 1][j] == 0) {
-                dp[m - 1][j] = dp[m - 1][j + 1];
-            }
-        }
-
-        for (int i = m - 2; i >= 0; i--) {
-            if (obstacleGrid[i][n - 1] == 0) {
-                dp[i][n - 1] = dp[i + 1][n - 1];
-            }
-        }
-
-        for(int i = m - 2; i >= 0; i--) {
-            for(int j = n - 2; j >= 0; j--) {
-                if(obstacleGrid[i][j] == 0) {
-                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
-                } else {
-                    dp[i][j] = 0;
+        // Use a 1D vector to store path counts for the current row
+        vector<long long> dp(n, 0);
+        
+        // Base case: if start is not blocked
+        dp[n - 1] = (obstacleGrid[m - 1][n - 1] == 0) ? 1 : 0;
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else if (j + 1 < n) {
+                    dp[j] += dp[j + 1];
                 }
             }
         }
-        return dp[0][0];
+        return (int)dp[0];
     }
 };
