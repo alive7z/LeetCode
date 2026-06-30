0b5f924db4d3d3a54682c65b23decba9f284a95d
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 17:23:27 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths.cpp
+++ dynamic-programming/unique-paths.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int a = m - n - 2;
+        int a = m + n - 2;
         int b = min(m - 1, n - 1);
-        int res = 1;
+        long long res = 1;
         for(int i = 1; i <= b; i++) {
-            res *= (a - b + i)) / i;
+            res = res * (a - b + i) / i;
         }
-        return res;
+        return (int)res;
     }
 };
