dfc936c7fd295aba4fde75ec52705f5d6b2c8078
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-06-30 17:43:47 +0530

    LC-unique-paths-ii [Runtime: 0 ms; Memory: 11.7 MB]

--- array/unique-paths-ii.cpp
+++ array/unique-paths-ii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int combinations(vector<vector<int>>& obstacleGrid, int i, int j, int m, int n) {
+    int combinations(vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp, int i, int j, int m, int n) {
         if(i >= m || j >= n || obstacleGrid[i][j] == 1) return 0;
         if(i == m - 1 && j == n - 1) return 1;
-        return combinations(obstacleGrid, i + 1, j, m, n) + combinations(obstacleGrid, i, j + 1, m, n);
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = combinations(obstacleGrid, dp, i + 1, j, m, n) + combinations(obstacleGrid, dp, i, j + 1, m, n);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        return combinations(obstacleGrid, 0, 0, m, n);
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return combinations(obstacleGrid, dp, 0, 0, m, n);
     }
 };
