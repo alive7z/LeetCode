f9f14d5331b0d31bc7dcaad37aee71fb5f89dbb3
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-01 11:18:31 +0530

    LC-partition-equal-subset-sum [Runtime: 200 ms; Memory: 17.4 MB]

--- array/partition-equal-subset-sum.cpp
+++ array/partition-equal-subset-sum.cpp
@@ -10,10 +10,6 @@ public:
         int target = sum / 2;
         vector<vector<bool>> dp(m, vector<bool> (target+1, false));
 
-        for(int i = 0; i < m; i++) dp[i][0] = true;
-
-        if(nums[0] <= target) dp[0][nums[0]] = true;
-
         for(int i = 0; i < m; i++) {
             for(int j = 0; j <= target; j++) {
                 if(i == 0) {
