60d303b13d62105f65fc88a70e527cbda5186bfc
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-01 17:54:35 +0530

    LC-coin-change [Runtime: N/A; Memory: N/A]

--- breadth-first-search/coin-change.cpp
+++ breadth-first-search/coin-change.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void combinations(vector<int>& coins, int amount, int cnt, int i, int& ans) {
-        if (cnt >= ans) return;
-        if(i == coins.size()) {
-            if(amount == 0) {
-                ans = min(ans, cnt);
-            }
-            return;
-        }
-        if(coins[i] <= amount) {
-            combinations(coins, amount - coins[i], cnt + 1, i, ans);
-        }
-        combinations(coins, amount, cnt, i + 1, ans);
-    }
     int coinChange(vector<int>& coins, int amount) {
-        if(coins.size() == 0 && amount > 0) return -1;
-        if(amount == 0) return 0;
-        int cnt = 0, ans = INT_MAX;
-        combinations(coins, amount, cnt, 0, ans);
-        return ans == INT_MAX ? -1 : ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount + 1, 0));
+
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+            else
+                dp[0][j] = 1e9;
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= amount; j++) {
+                    int pick = 1e9, notPick = 1e9;
+                    if(coins[i] <= j) {
+                        pick = 1 + dp[i][j-coins[i]];
+                    }
+                    notPick = dp[i-1][j];
+                    dp[i][j] = min(pick, notPick);
+                }
+            }
+        }
+        int ans = dp[n-1][amount];
+        return (ans >= 1e9) ? -1 : ans;
     }
 };
