8ba0698b30f7318fc356b33e1b819758e0d0d66a
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-01 18:46:00 +0530

    LC-coin-change [Runtime: N/A; Memory: N/A]

--- dynamic-programming/coin-change.cpp
+++ dynamic-programming/coin-change.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount + 1, 0));
-
-        for (int j = 0; j <= amount; j++) {
-            if (j % coins[0] == 0)
-                dp[0][j] = j / coins[0];
-            else
-                dp[0][j] = 1e9;
-        }
-
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j <= amount; j++) {
-                    int pick = 1e9, notPick = 1e9;
-                    if(coins[i] <= j) {
-                        pick = 1 + dp[i][j-coins[i]];
-                    }
-                    notPick = dp[i-1][j];
-                    dp[i][j] = min(pick, notPick);
-                }
+    int minimumNumberOfCoins(vector<int>& coins, int amount, int i) {
+        if(i == 0) {
+            if(amount % coins[i] == 0) {
+                return amount / coins[i];
             }
+            return 1e9;
         }
-        int ans = dp[n-1][amount];
-        return (ans >= 1e9) ? -1 : ans;
+        if(amount == 0) return 0;
+        int notPick = minimumNumberOfCoins(coins, amount, i - 1);
+        int pick = INT_MAX;
+        if(amount >= coins[i]) {
+            pick = minimumNumberOfCoins(coins, amount - coins[i], i);
+        }
+        return min(pick, notPick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int ans = minimumNumberOfCoins(coins, amount, coins.size() - 1);
+        if(ans >= 1e9) return -1;
+        return ans;
     }
 };
