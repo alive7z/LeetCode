f9536f8ecbcb8071a6a2513bd401615c3db693dc
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-02 18:49:31 +0530

    LC-find-a-safe-walk-through-a-grid [Runtime: N/A; Memory: N/A]

--- shortest-path/find-a-safe-walk-through-a-grid.cpp
+++ shortest-path/find-a-safe-walk-through-a-grid.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
-    bool safePath(vector<vector<int>>& grid, int health, vector<vector<bool>>& dp, vector<vector<int>>& visited, int i, int j, int m, int n) {
-        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j]) {
+    bool dfs(vector<vector<int>>& grid, int i, int j, int health,
+             vector<vector<vector<int>>>& dp,
+             vector<vector<bool>>& visited) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j])
             return false;
-        }
-        health = health - grid[i][j];
-        if(health == 0) return false;
-        if(i == m - 1 && j == n - 1) return dp[i][j] = true;
+
+        health -= grid[i][j];
+
+        if (health <= 0)
+            return false;
+
+        if (i == m - 1 && j == n - 1)
+            return true;
+
+        if (dp[i][j][health] != -1)
+            return dp[i][j][health];
+
         visited[i][j] = true;
-        if(dp[i][j] != false) return dp[i][j];
-        bool down = safePath(grid, health, dp, visited, i + 1, j, m, n);
-        bool up = safePath(grid, health, dp, visited, i - 1, j, m, n);
-        bool right = safePath(grid, health, dp, visited, i, j + 1, m, n);
-        bool left = safePath(grid, health, dp, visited, i, j - 1, m, n);
+
+        bool ans = dfs(grid, i + 1, j, health, dp, visited) ||
+                   dfs(grid, i - 1, j, health, dp, visited) ||
+                   dfs(grid, i, j + 1, health, dp, visited) ||
+                   dfs(grid, i, j - 1, health, dp, visited);
+
         visited[i][j] = false;
-        return dp[i][j] = down || up || right || left;
+
+        return dp[i][j][health] = ans;
     }
+
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> visited(m, vector<int> (n, false));
-        vector<vector<bool>> dp(m, vector<bool> (n, false));
-        return safePath(grid, health, dp, visited, 0, 0, m, n);
+
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(health + 1, -1)));
+
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        return dfs(grid, 0, 0, health, dp, visited);
     }
 };
