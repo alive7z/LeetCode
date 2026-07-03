f649236663d763736ff1f03317ddd02d8ee1ecd8
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-03 18:28:40 +0530

    LC-coin-change-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/coin-change-ii.cpp
+++ dynamic-programming/coin-change-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    long long change(int amount, vector<int>& coins) {
+    int change(int amount, vector<int>& coins) {
         int n = coins.size();
         if(n == 0) return 0;
 
-        vector<vector<long long>> dp(n, vector<long long> (amount + 1, 0));
+        vector<int> prev(amount + 1, 0);
         
         for(int a = 0; a <= amount; a++) {
             if(a % coins[0] == 0) {
-                dp[0][a] = 1;
+                prev[a] = 1;
             }
         }
 
         for(int i = 1; i < n; i++) {
+            vector<int> curr(amount + 1, 0);
             for(int j = 0; j <= amount; j++) {
-                long long notPick = dp[i-1][j];
-                long long pick = 0;
+                int notPick = prev[j];
+                int pick = 0;
                 if(coins[i] <= j) {
-                    pick = dp[i][j - coins[i]];
+                    pick = curr[j - coins[i]];
                 }
-                dp[i][j] = pick + notPick;
+                curr[j] = pick + notPick;
             }
+            prev = curr;
         }
 
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
