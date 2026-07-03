cd72b2f55b3317dfeb900156bd973d86251767c9
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-03 11:29:34 +0530

    LC-target-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/target-sum.cpp
+++ dynamic-programming/target-sum.cpp
@@ -9,29 +9,31 @@ public:
         if((totalSum - target) % 2 != 0 || (totalSum - target) < 0) return 0;
         int goal = (totalSum - target) / 2;
 
-        vector<vector<int>> dp(n, vector<int> (goal + 1, 0));
+        vector<int> prev(goal + 1, 0);
         for(int i = 0; i <= goal; i++) {
             if(nums[0] == 0 && i == 0) {
-                dp[0][i] = 2;
+                prev[i] = 2;
                 continue;
             } else if(i == 0 || nums[0] == i) {
-                dp[0][i] = 1;
+                prev[i] = 1;
             } else {
-                dp[0][i] = 0;
+                prev[i] = 0;
             }
         }
 
         for(int i = 1; i < n; i++) {
+            vector<int> curr(goal + 1, 0);
             for(int j = 0; j <= goal; j++) {
                 int pick = 0;
                 if(nums[i] <= j) {
-                    pick = dp[i-1][j-nums[i]];
+                    pick = prev[j-nums[i]];
                 }
-                int notPick = dp[i-1][j];
-                dp[i][j] = pick + notPick;
+                int notPick = prev[j];
+                curr[j] = pick + notPick;
             }
+            prev = curr;
         }
         
-        return dp[n-1][goal];
+        return prev[goal];
     }
 };
