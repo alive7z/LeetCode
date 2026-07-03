10e7eef1f277b4e8e8491362fc7f02f3f88b8883
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-03 11:27:55 +0530

    LC-target-sum [Runtime: N/A; Memory: N/A]

--- array/target-sum.cpp
+++ array/target-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    int ways(vector<int>& nums, int target, int sum, int i) {
-        if (i == 0) {
-            int cnt = 0;
-            if (sum + nums[0] == target) cnt++;
-            if (sum - nums[0] == target) cnt++;
-            return cnt;
-        }
-        int plus = ways(nums, target, sum + nums[i], i - 1);
-        int minus = ways(nums, target, sum - nums[i], i - 1);
-        return plus + minus;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        return ways(nums, target, sum, nums.size() - 1);
+        int n = nums.size();
+        int totalSum = 0;
+        for(int val : nums) {
+            totalSum += val;
+        }
+        if((totalSum - target) % 2 != 0 || (totalSum - target) < 0) return 0;
+        int goal = (totalSum - target) / 2;
+
+        vector<vector<int>> dp(n, vector<int> (goal + 1, 0));
+        for(int i = 0; i <= goal; i++) {
+            if(nums[0] == 0 && i == 0) {
+                dp[0][i] = 2;
+                continue;
+            } else if(i == 0 || nums[0] == i) {
+                dp[0][i] = 1;
+            } else {
+                dp[0][i] = 0;
+            }
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= goal; j++) {
+                int pick = 0;
+                if(nums[i] <= j) {
+                    pick = dp[i-1][j-nums[i]];
+                }
+                int notPick = dp[i-1][j];
+                dp[i][j] = pick + notPick;
+            }
+        }
+        
+        return dp[n-1][goal];
     }
 };
