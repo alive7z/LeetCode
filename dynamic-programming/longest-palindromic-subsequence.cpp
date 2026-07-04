3cbba89059f8b4e5f3e5db81aac4fa3454243d12
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-04 17:18:00 +0530

    LC-longest-palindromic-subsequence [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/longest-palindromic-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int palindrome(string s, int i, int j) {
+        if(i > j) {
+            return 0;
+        }
+        if(s[i] == s[j]) {
+            return 2 + palindrome(s, i + 1, j - 1);
+        }
+        return max(palindrome(s, i + 1, j), palindrome(s, i, j - 1));
+    }
+    int longestPalindromeSubseq(string s) {
+        return palindrome(s, 0, s.size() - 1);
+    }
+};
