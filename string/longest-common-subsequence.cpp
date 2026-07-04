8c1d67412d9c7259f172f704d0c08aa5f171e42a
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-04 11:40:37 +0530

    LC-longest-common-subsequence [Runtime: N/A; Memory: N/A]

---
+++ string/longest-common-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<int> prev(n + 1, 0);
+
+        for(int i = 0; i <= m; i++) prev[0] = 0;
+
+        for(int i = 1; i <= m; i++) {
+            vector<int> curr(n + 1, 0);
+            for(int j = 1; j <= n; j++) {
+                if(text1[i-1] == text2[j-1]) {
+                    curr[j] = 1 + prev[j-1];
+                } else {
+                    curr[j] = max(prev[j], curr[j-1]);
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+};
