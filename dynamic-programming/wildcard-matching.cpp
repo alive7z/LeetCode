80eea19d4bc828f051118d3922fd1bf2d43be03e
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-06 23:44:24 +0530

    LC-wildcard-matching [Runtime: N/A; Memory: N/A]

--- dynamic-programming/wildcard-matching.cpp
+++ dynamic-programming/wildcard-matching.cpp
@@ -4,30 +4,30 @@ public:
         int m = s.size();
         int n = p.size();
 
-        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
-        dp[0][0] = true;
+        vector<int> prev(n + 1, false), curr(n + 1, false);
+        prev[0] = true;
 
         for(int i = 1; i <= m; i++) {
-            dp[i][0] = false;
+            curr[0] = false;
         }
 
         for(int j = 1; j <= n; j++) {
-            if(p[j-1] == '*') dp[0][j] = dp[0][j-1];
-            else dp[0][j] = false;
+            if(p[j-1] == '*') prev[j] = prev[j-1];
+            else prev[j] = false;
         }
 
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
                 if(s[i-1] == p[j-1] || p[j-1] == '?') {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 } else if(p[j-1] == '*') {
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    curr[j] = prev[j] || curr[j-1];
                 } else {
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }
             }
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
