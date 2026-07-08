5c797bb5d45aab4d0d4803c8e152401f6c74a0b6
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-08 13:58:10 +0530

    LC-balanced-binary-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/balanced-binary-tree.cpp
+++ binary-tree/balanced-binary-tree.cpp
@@ -17,7 +17,7 @@ public:
         }
         int left = check(root -> left);
         int right = check(root -> right);
-        
+        if(left == -1 || right == -1) return -1;
         if(abs(left - right) > 1) return -1;
         return 1 + max(left, right);
     }
