cf3dfd9be1e70f9030c2c059912ea9fdd84fbb47
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-09 17:06:31 +0530

    LC-symmetric-tree [Runtime: N/A; Memory: N/A]

--- depth-first-search/symmetric-tree.cpp
+++ depth-first-search/symmetric-tree.cpp
@@ -14,7 +14,6 @@ public:
     bool check(TreeNode* p, TreeNode* q) {
         if(p == NULL && q == NULL) return true;
         if(p == NULL || q == NULL) return false;
-        if(p -> val == q -> val) return true;
         if(p -> val != q -> val) return false;
         return check(p->left, q->right) && check(p->right, q->left);
     }
