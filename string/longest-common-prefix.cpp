b1730a92b369f8ecc579c620ab20ad98716a00d9
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-11 19:31:30 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

---
+++ string/longest-common-prefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        sort(strs.begin(), strs.end());
+        for(int idx = 1; idx < strs.size(); idx++) {
+            string first = strs[idx-1];
+            string second = strs[idx];
+            int i = 0;
+            string temp;
+            while(i < first.size() && i < second.size()) {
+                if(first[i] == second[i]) {
+                    temp += first[i];
+                } else break;
+                i++;
+            }
+            if(ans.size() == 0) {
+                ans = temp; continue;
+            }
+            if(temp.size() > 0) {
+                if(ans.size() > temp.size()) ans = temp;
+            } else return "";
+        }
+        return ans;
+    }
+};
