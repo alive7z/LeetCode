d6729beb196542e79745c2aead5409fa7c62dc04
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-07-26 22:59:57 +0530

    LC-maximum-product-of-three-numbers [Runtime: N/A; Memory: N/A]

--- sorting/maximum-product-of-three-numbers.cpp
+++ sorting/maximum-product-of-three-numbers.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int first = nums[0], second = -1001, third = -1001;
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] > first) {
-                third = second;
-                second = first;
-                first = nums[i];
-            } else if(nums[i] > second) {
-                third = second;
-                second = nums[i];
-            } else if(nums[i] > third) {
-                third = nums[i];
-            } else continue;
-        }
-        return first * second * third;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max(abs(nums[0]) * abs(nums[1]) * nums[n-1], nums[n-1] * nums[n-2] * nums[n-3]);
     }
 };
