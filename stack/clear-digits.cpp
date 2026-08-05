db4865d29f1e285010d4101f32d57e4ef55a43a0
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-08-06 00:11:36 +0530

    LC-clear-digits [Runtime: 0 ms; Memory: 8.3 MB]

--- stack/clear-digits.cpp
+++ stack/clear-digits.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        string str = "";
-        vector<char> store;
-        for(char c : s) {
-            if(c >= '0' && c <= '9') {
-                if(!store.empty()) store.pop_back();
-            } else store.push_back(c);
+        string ans;
+
+        for (char c : s) {
+            if (isdigit(c))
+                ans.pop_back();
+            else
+                ans.push_back(c);
         }
-        for(char c : store) {
-            str += c;
-        }
-        return str;
+
+        return ans;
     }
 };
