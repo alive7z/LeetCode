308b9710e94ead2d3280a0ac1c92fd7ebcd76e0d
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-08-18 23:21:17 +0530

    LC-find-the-largest-almost-missing-integer [Runtime: N/A; Memory: N/A]

--- hash-table/find-the-largest-almost-missing-integer.cpp
+++ hash-table/find-the-largest-almost-missing-integer.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    int largestInteger(vector<int>& nums, int k) {
-        if(k == 0) return -1;
+    int largestInteger(vector<int>& A, int k) {
+        int f[51] = {0};
+        for (auto& x : A)
+            f[x]++;
 
-        if(k == 1) {
-            vector<int> store(51);
-            for(int val : nums) {
-                store[val]++;
-            }
-            for(int i = 50; i >= 0; i--) {
-                if(store[i] == 1) return i;
-            }
-            return -1;
-        }
+        int res = -1, n = A.size();
+        for (int i = 0; i < n; i++)
+            if (k == n || (f[A[i]]==1 && (k==1||!i||i==n-1)))
+                res = max(res, A[i]);
 
-        if (k == nums.size()) {
-            return *max_element(nums.begin(), nums.end());
-        }
-
-        int a = 0, b = 0;
-
-        for(int val : nums) {
-            if(val == nums[0]) a++;
-            if(val == nums[nums.size() - 1]) b++;
-        }
-
-        if(a == 1 && b > 1) return nums[0];
-
-        if(a > 1 && b == 1) return nums[nums.size() - 1];
-
-        return max(nums[0], nums[nums.size() - 1]);
+        return res;
     }
 };
