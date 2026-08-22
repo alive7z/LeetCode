b8b0b830cfbe8fd74d5b334ad81e28cc0419ba33
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-08-22 11:53:10 +0530

    LC-set-matrix-zeroes [Runtime: N/A; Memory: N/A]

--- hash-table/set-matrix-zeroes.cpp
+++ hash-table/set-matrix-zeroes.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row = matrix.size(), col = matrix[0].size();
-        vector<bool> r(row, false), c(col, false);
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
+        vector<int> r(matrix.size(), false), c(matrix[0].size(), false);
+        for(int i = 0; i < matrix.size(); i++) {
+            for(int j = 0; j < matrix[0].size(); j++) {
                 if(matrix[i][j] == 0) {
                     r[i] = true;
                     c[j] = true;
                 }
             }
         }
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
+        for(int i = 0; i < matrix.size(); i++) {
+            for(int j = 0; j < matrix[0].size(); j++) {
                 if(r[i] || c[j]) {
                     matrix[i][j] = 0;
                 }
