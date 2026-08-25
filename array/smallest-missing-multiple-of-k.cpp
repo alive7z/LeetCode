1a00394933bba239b8000407d274cd86540a4ee8
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-08-25 20:11:43 +0530

    LC-smallest-missing-multiple-of-k [Runtime: N/A; Memory: N/A]

--- array/smallest-missing-multiple-of-k.cpp
+++ array/smallest-missing-multiple-of-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         vector<int> store(100, 0);
-        int n = k;
+        int n = 0;
         for(int val : nums) {
             if(val % k == 0) {
                 store[val-1]++;
@@ -12,7 +12,7 @@ public:
             if(store[i] == 0 && (i + 1) % k == 0) return i + 1;
         }
         while(n < 100) {
-            n += n;
+            n += k;
         }
         return n;
     }
