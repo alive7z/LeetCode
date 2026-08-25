537ee571be597b5e66033adced6ace4ca4763f4d
Sumit Singh Bagdwal <sumitsbagdwal.github@gmail.com>
2026-08-25 20:07:02 +0530

    LC-smallest-missing-multiple-of-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        vector<int> store(100, 0);
+        for(int val : nums) {
+            if(val % k == 0) {
+                store[val-1]++;
+            }
+        }
+        for(int i = 0; i < store.size(); i++) {
+            if(store[i] == 0 && (i + 1) % k == 0) return i + 1;
+        }
+        return 0;
+    }
+};
